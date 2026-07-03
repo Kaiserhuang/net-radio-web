31a73761f02f49e19ed9aab608805cf9d10b5c21
Kaiserhuang <kaiserhuang123@hotmail.com>
2026-07-03 12:41:23 +0800

    fix: rename asgi_app to app for Vercel ASGI detection

--- api/index.py
+++ api/index.py
@@ -3,4 +3,4 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ["VERCEL"] = "1"
 os.environ["DB_PATH"] = "/tmp/net_radio.db"
 
-from server import app as asgi_app
+from server import app
